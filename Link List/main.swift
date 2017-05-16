c7d18a00391fe115c07c9e5237d2508851d8e0da
Jelani <jelani@Jelanis-Mac.local>
2017-05-16 15:45:47 -0400

    Initial Commit

---
+++ Link List/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Link List
+//
+//  Created by Jelani on 16/05/2017.
+//  Copyright © 2017 Tap. TOUCH. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
